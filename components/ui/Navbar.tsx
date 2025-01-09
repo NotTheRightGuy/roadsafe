58788c4e876d7d9c06cb0b6c5b65f5c1b604b5fe
ni3rav <mahetan35@gmail.com>
2025-01-09 18:40:42 +0530

    minor fixes

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -4,13 +4,14 @@ import Logo from "./Logo";
 
 function Navbar() {
   return (
-    <header className="absolute right-0 top-0 flex h-fit z-10 w-full items-center justify-between py-2 px-1">
-      <div className="scale-75">
+    <header className="flex h-fit z-10 w-full items-center justify-between py-2 px-1 overflow-hidden">
+      <div className="flex items-center justify-center">
         <Logo />
+        <h1 className="font-black text-2xl">Roadsafe</h1>
       </div>
-      <nav className="flex flex-wrap items-center justify-center gap-4 w-56">
-        <Link href="/signup" className="py-2  text-zinc-950 font-semibold">Sign Up</Link>
-        <Link href="/login" className="py-2 px-4 rounded-lg bg-red-700 text-zinc-50">Login</Link>
+      <nav className="flex flex-wrap items-center justify-center gap-4 mr-2">
+        {/* <Link href="/signup" className="py-2  text-zinc-950 font-semibold">Sign Up</Link> */}
+        <Link href="/login" className="py-2 px-4 rounded-lg bg-red-700 text-zinc-50">SignUp</Link>
       </nav>
     </header>
   );
