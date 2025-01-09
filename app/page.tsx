b176be9d7258ab8befb43808af2e48b6c9cb5555
ni3rav <mahetan35@gmail.com>
2025-01-10 05:25:02 +0530

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import Section1 from "@/components/LandingPage/Section1";
 import Section2 from "@/components/LandingPage/Section2";
 import Section3 from "@/components/LandingPage/Section3";
-import Section4 from "@/components/LandingPage/Section4";
 import Footer from "@/components/LandingPage/Footer";
 
 function Home() {
@@ -11,7 +10,6 @@ function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
-      <Section4 />
       <Footer />
     </div>
   );
