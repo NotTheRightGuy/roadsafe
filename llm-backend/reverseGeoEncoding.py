c817ed2e805ea787374842fbaf4dd467bbefaeb0
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2025-01-10 10:09:24 +0530

    chore: removed llm-backend folder from repo

--- llm-backend/reverseGeoEncoding.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def reverseGeoEncoding(latitude, longitude):
-  url = f"https://api.olamaps.io/places/v1/reverse-geocode?latlng={latitude},{longitude}&api_key=pHQXmsEPbAUtk9xudUsWl5TUI28v55RknfVLGP7p"
-  response = requests.get(url)
-  data = response.json()
-  return data['results'][0]['formatted_address']
